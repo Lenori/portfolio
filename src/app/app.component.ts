39937eab97acc27fbcc9ef7de98642ba2f888f40
Leo Lenori <leolenori@gmail.com>
2019-07-29 15:22:26 -0300

    backup 29/07/2019 15:22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MouseDirective } from './mouse.directive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
   title = 'lenori';
+  animatedId = 1;
+  sections: any;
+
+  mouseWheelUpFunc() {
+    document.getElementById('container').scrollBy(-1900, 0);
+    this.hideInfos(1);
+    if (this.animatedId !== 1) { this.animatedId--; }
+    this.showInfos(1);
+  }
+
+  mouseWheelDownFunc() {
+    document.getElementById('container').scrollBy(1900, 0);
+    if (this.animatedId !== this.sections) { this.animatedId++; }
+  }
+
+  showInfos(id) {
+
+    const elem = document.getElementById('container-0' + id);
+    window.setTimeout(() => { elem.classList.toggle('is-visible'); }, 500);
+
+  }
+
+  hideInfos(id) {
+
+    document.getElementById('container-0' + id).classList.remove('is-visible');
+
+  }
+
+  ngOnInit() {
+
+    this.showInfos(this.animatedId);
+    this.sections = document.querySelectorAll('#container .sections').length;
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MouseDirective} from './mouse.directive';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MouseDirective,
+    HomeComponent
   ],
   imports: [
     BrowserModule
