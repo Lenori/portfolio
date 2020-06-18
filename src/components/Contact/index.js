22ad72b99bda2530ca8ff98d607518797755c98c
Leo Lenori <leolenori@gmail.com>
2020-06-18 03:19:44 -0300

    remove unused css vars

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,8 +8,8 @@ class Contact extends Component {
     render() {
         return(
             <Content>
-                <a target="_blank" href="https://wa.me/5561982889782?text=Olá!%20Vi%20seus%20ótimos%20sites%20e%20gostaria%20de%20ser%20seu%20próximo%20cliente!"><FaWhatsapp size={18} color={'#f5f5f5'} /></a>
-                <a target="_blank" href="mailto:leolenori@gmail.com"><FaEnvelope size={18} color={'#f5f5f5'} /></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://wa.me/5561982889782?text=Olá!%20Vi%20seus%20ótimos%20sites%20e%20gostaria%20de%20ser%20seu%20próximo%20cliente!"><FaWhatsapp size={18} color={'#f5f5f5'} /></a>
+                <a target="_blank" rel="noopener noreferrer" href="mailto:leolenori@gmail.com"><FaEnvelope size={18} color={'#f5f5f5'} /></a>
             </Content>
         )
     }
